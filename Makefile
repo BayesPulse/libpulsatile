0b5c375da70578c25b737ff4363cc0ed012f4770
Matt <mjmulvahill@gmail.com>
2017-12-21 15:46:56 -0700

    testing functioning -- now debugging counter class.

---
+++ Makefile
@@ -0,0 +1,35 @@
+#
+# Makefile for libpulsatile
+#
+ 
+CC := g++ # This is the main compiler
+# CC := clang --analyze # and comment out the linker last line for sanity
+SRCDIR := src
+BUILDDIR := build
+TARGET := bin/libpulsatile
+ 
+SRCEXT := cpp
+SOURCES := $(shell find $(SRCDIR) -type f -name *.$(SRCEXT))
+OBJECTS := $(patsubst $(SRCDIR)/%,$(BUILDDIR)/%,$(SOURCES:.$(SRCEXT)=.o))
+CFLAGS := -g # -Wall
+LIB := $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)
+INC := -I include -I../inst/include -DARMA_64BIT_WORD -std=c++11
+CXX_STD = CXX11
+
+$(TARGET): $(OBJECTS)
+	@echo " Linking..."
+	@echo " $(CC) $^ -o $(TARGET) $(LIB)"; $(CC) $^ -o $(TARGET) $(LIB)
+
+$(BUILDDIR)/%.o: $(SRCDIR)/%.$(SRCEXT)
+	@mkdir -p $(BUILDDIR)
+	@echo " $(CC) $(CFLAGS) $(INC) -c -o $@ $<"; $(CC) $(CFLAGS) $(INC) -c -o $@ $<
+
+clean:
+	@echo " Cleaning...";
+	@echo " $(RM) -r $(BUILDDIR) $(TARGET)"; $(RM) -r $(BUILDDIR) $(TARGET)
+
+# Tests
+tester:
+	$(CC) $(CFLAGS) tests/proposalvariance_tests.cpp $(INC) $(LIB) -o bin/proposalvariance_tests
+
+.PHONY: clean
--- tests/proposalvariance_tests.cpp
+++ tests/proposalvariance_tests.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include "catch.h"
 #include <armadillo>
 #include "counter.h"
 #include "proposalvariance.h"
@@ -13,29 +13,46 @@ TEST_CASE( "Counter class", "[counter]" ) {
 
   Counter cnt;
 
+  cnt.addreject();
   SECTION( "can iterate/add reject" ) {
-    cnt.addreject();
 
     REQUIRE(cnt.getiter() == 1);
     REQUIRE(cnt.getaccept() == 0);
     REQUIRE(cnt.getratio() == 0);
   }
 
+  cnt.addaccept();
   SECTION( "can add accept" ) {
-    cnt.addaccept();
 
     REQUIRE( cnt.getaccept() == 1   );
     REQUIRE( cnt.getiter()   == 2   );
     REQUIRE( cnt.getratio()  == 0.5 );
   }
 
+  cnt.resetratio();
   SECTION( "can reset" ) {
-    cnt.resetratio();
 
     REQUIRE( cnt.getaccept() == 0 );
     REQUIRE( cnt.getiter()   == 0 );
     REQUIRE( cnt.getratio()  != cnt.getratio() );
   }
+
+  SECTION( "1-parameter version, core functions." ) {
+
+    REQUIRE( cnt.getaccept() == 0 );
+    REQUIRE( cnt.getiter() == 0 );
+
+    for (int i = 0; i < 100; ++i) {
+      if (i % 4 == 0) cnt.addaccept();
+      cnt.addreject();
+    }
+
+    // double check math on this..
+    REQUIRE( cnt.getaccept() == 25 );
+    REQUIRE( cnt.getiter() == 100 );
+    REQUIRE( cnt.getratio() == 0.25 );
+  }
+
 
 }
 
@@ -56,24 +73,24 @@ TEST_CASE( "ProposalVariance class (1 param)", "[ProposalVariance]" ) {
     REQUIRE( pv.getpsd() == sqrt(0.3) );
   }
 
+  pv.addreject();
   SECTION( "can iterate/add reject" ) {
-    pv.addreject();
 
     REQUIRE(pv.getiter() == 1);
     REQUIRE(pv.getaccept() == 0);
     REQUIRE(pv.getratio() == 0);
   }
 
+  pv.addaccept();
   SECTION( "can add accept" ) {
-    pv.addaccept();
 
     REQUIRE( pv.getaccept() == 1   );
     REQUIRE( pv.getiter()   == 2   );
     REQUIRE( pv.getratio()  == 0.5 );
   }
 
+  pv.resetratio();
   SECTION( "can reset" ) {
-    pv.resetratio();
 
     REQUIRE( pv.getaccept() == 0 );
     REQUIRE( pv.getiter()   == 0 );
@@ -81,19 +98,18 @@ TEST_CASE( "ProposalVariance class (1 param)", "[ProposalVariance]" ) {
   }
 
   SECTION( "1-parameter version, core functions." ) {
-    ProposalVariance pv(0.7, 500, 25000, 0.35);
+    ProposalVariance pv2(0.7, 500, 25000, 0.35);
 
-    REQUIRE( pv.getpv() == 0.7 );
-    REQUIRE( pv.getpsd() == sqrt(0.7) );
+    REQUIRE( pv2.getpv() == 0.7 );
+    REQUIRE( pv2.getpsd() == sqrt(0.7) );
 
-    int i;
-    for (int i = 0; i < 100; i++) {
-      if (i % 4 == 0) pv.addaccept();
-      pv.addreject();
+    for (int i = 0; i < 100; ++i) {
+      if (i % 4 == 0) pv2.addaccept();
+      pv2.addreject();
     }
 
     // double check math on this..
-    REQUIRE( pv.getratio() == 0.2 );
+    REQUIRE( pv2.getratio() == 0.25 );
   }
 
 }
@@ -105,75 +121,75 @@ TEST_CASE( "ProposalVariance class (1 param)", "[ProposalVariance]" ) {
 // Test the ProposalVariance2p class
 //
 
-TEST_CASE( "ProposalVariance class (2 param)", "[ProposalVariance2p]" ) {
-
-  arma::vec initial_pvs = { 0.7, 0.1 };
-  ProposalVariance2p pv(initial_pvs, 500, 25000, 0.25);
-
-  SECTION( "can initialize pv and psd matrices" ) {
-    double x, y, xy;
-    x = 0.7; y = 0.1; xy = -0.9 * sqrt(x * y);
-    arma::mat checkpv = { { x, xy }, { xy, y } };
-    arma::mat checkchol = chol(checkpv);
-
-    REQUIRE( arma::approx_equal(pv.getpv(), checkpv, "absdiff", 0.0000001) );
-    REQUIRE( arma::approx_equal(pv.getpsd(), checkchol, "absdiff", 0.0000001) );
-  }
-
-  SECTION( "can iterate/add reject" ) {
-    pv.addreject();
-
-    REQUIRE(pv.getiter() == 1);
-    REQUIRE(pv.getaccept() == 0);
-    REQUIRE(pv.getratio() == 0);
-  }
-
-  SECTION( "can add accept" ) {
-    pv.addaccept();
-
-    REQUIRE( pv.getaccept() == 1   );
-    REQUIRE( pv.getiter()   == 2   );
-    REQUIRE( pv.getratio()  == 0.5 );
-  }
-
-  SECTION( "can reset" ) {
-    pv.resetratio();
-
-    REQUIRE( pv.getaccept() == 0 );
-    REQUIRE( pv.getiter()   == 0 );
-    REQUIRE( pv.getratio()  != pv.getratio() );
-  }
-
-
-  SECTION( "2-parameter version, core functions." ) {
-
-    // Initialize object for testing
-    arma::vec initial_pvs = { 0.7, 0.1 };
-    ProposalVariance2p pv(initial_pvs, 500, 25000, 0.25);
-    arma::mat initial_mat = pv.getpv();
-
-    int i;
-    for (int i = 0; i < 100; i++) {
-      pv.addreject();
-      if (i % 4 == 0) pv.addaccept();
-    }
-
-    // // double check math on this..
-    // REQUIRE( pv.getaccept() == 20 );
-    // REQUIRE( pv.getiter() == 100 );
-    // REQUIRE( pv.getratio() == 0.2 );
-
-    // // Test adjustpv();
-    // pv.adjustpv(-0.90);
-    // REQUIRE( arma::approx_equal(pv.getpv(), initial_mat, "absdiff", 0.0000001) );
-
-    // //double y = 1.0 + 1000.0 * pow(0.2 - 0.25, 3);
-    // //if (y < 0.9) y = 0.9;
-    // //if (y > 1.1) y = 1.1;
-    // //if (y == 1.1 | y == 0.9) {
-    // //  pv = pv % mydiag;
-    // //}
-
-  }
-
-}
+//TEST_CASE( "ProposalVariance class (2 param)", "[ProposalVariance2p]" ) {
+//
+//  arma::vec initial_pvs = { 0.7, 0.1 };
+//  ProposalVariance2p pv(initial_pvs, 500, 25000, 0.25);
+//
+//  SECTION( "can initialize pv and psd matrices" ) {
+//    double x, y, xy;
+//    x = 0.7; y = 0.1; xy = -0.9 * sqrt(x * y);
+//    arma::mat checkpv = { { x, xy }, { xy, y } };
+//    arma::mat checkchol = chol(checkpv);
+//
+//    REQUIRE( arma::approx_equal(pv.getpv(), checkpv, "absdiff", 0.0000001) );
+//    REQUIRE( arma::approx_equal(pv.getpsd(), checkchol, "absdiff", 0.0000001) );
+//  }
+//
+//  SECTION( "can iterate/add reject" ) {
+//    pv.addreject();
+//
+//    REQUIRE(pv.getiter() == 1);
+//    REQUIRE(pv.getaccept() == 0);
+//    REQUIRE(pv.getratio() == 0);
+//  }
+//
+//  SECTION( "can add accept" ) {
+//    pv.addaccept();
+//
+//    REQUIRE( pv.getaccept() == 1   );
+//    REQUIRE( pv.getiter()   == 2   );
+//    REQUIRE( pv.getratio()  == 0.5 );
+//  }
+//
+//  SECTION( "can reset" ) {
+//    pv.resetratio();
+//
+//    REQUIRE( pv.getaccept() == 0 );
+//    REQUIRE( pv.getiter()   == 0 );
+//    REQUIRE( pv.getratio()  != pv.getratio() );
+//  }
+//
+//
+//  SECTION( "2-parameter version, core functions." ) {
+//
+//    // Initialize object for testing
+//    arma::vec initial_pvs = { 0.7, 0.1 };
+//    ProposalVariance2p pv(initial_pvs, 500, 25000, 0.25);
+//    arma::mat initial_mat = pv.getpv();
+//
+//    int i;
+//    for (int i = 0; i < 100; i++) {
+//      pv.addreject();
+//      if (i % 4 == 0) pv.addaccept();
+//    }
+//
+//    // // double check math on this..
+//    // REQUIRE( pv.getaccept() == 20 );
+//    // REQUIRE( pv.getiter() == 100 );
+//    // REQUIRE( pv.getratio() == 0.2 );
+//
+//    // // Test adjustpv();
+//    // pv.adjustpv(-0.90);
+//    // REQUIRE( arma::approx_equal(pv.getpv(), initial_mat, "absdiff", 0.0000001) );
+//
+//    // //double y = 1.0 + 1000.0 * pow(0.2 - 0.25, 3);
+//    // //if (y < 0.9) y = 0.9;
+//    // //if (y > 1.1) y = 1.1;
+//    // //if (y == 1.1 | y == 0.9) {
+//    // //  pv = pv % mydiag;
+//    // //}
+//
+//  }
+//
+//}
