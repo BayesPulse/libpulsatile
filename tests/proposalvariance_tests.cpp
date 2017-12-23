d10a937bfa7604e00e304968391a36842799121f
Matt <mjmulvahill@gmail.com>
2017-12-22 17:52:02 -0700

    working on remaining adjustpv test cases.

--- tests/proposalvariance_tests.cpp
+++ tests/proposalvariance_tests.cpp
@@ -108,7 +108,8 @@ TEST_CASE( "ProposalVariance class (1 param)", "[ProposalVariance]" ) {
       else pv2.addreject();
     }
 
-    // double check math on this..
+    REQUIRE( pv2.getaccept() == 25 );
+    REQUIRE( pv2.getiter() == 100 );
     REQUIRE( pv2.getratio() == 0.25 );
   }
 
@@ -136,60 +137,60 @@ TEST_CASE( "ProposalVariance class (2 param)", "[ProposalVariance2p]" ) {
     REQUIRE( arma::approx_equal(pv.getpsd(), checkchol, "absdiff", 0.0000001) );
   }
 
+  pv.addreject();
   SECTION( "can iterate/add reject" ) {
-    pv.addreject();
 
     REQUIRE(pv.getiter() == 1);
     REQUIRE(pv.getaccept() == 0);
     REQUIRE(pv.getratio() == 0);
   }
 
-//  SECTION( "can add accept" ) {
-//    pv.addaccept();
-//
-//    REQUIRE( pv.getaccept() == 1   );
-//    REQUIRE( pv.getiter()   == 2   );
-//    REQUIRE( pv.getratio()  == 0.5 );
-//  }
-//
-//  SECTION( "can reset" ) {
-//    pv.resetratio();
-//
-//    REQUIRE( pv.getaccept() == 0 );
-//    REQUIRE( pv.getiter()   == 0 );
-//    REQUIRE( pv.getratio()  != pv.getratio() );
-//  }
-//
-//
-//  SECTION( "2-parameter version, core functions." ) {
-//
-//    // Initialize object for testing
-//    arma::vec initial_pvs = { 0.7, 0.1 };
-//    ProposalVariance2p pv(initial_pvs, 500, 25000, 0.25);
-//    arma::mat initial_mat = pv.getpv();
-//
-//    int i;
-//    for (int i = 0; i < 100; i++) {
-//      pv.addreject();
-//      if (i % 4 == 0) pv.addaccept();
-//    }
-//
-//    // // double check math on this..
-//    // REQUIRE( pv.getaccept() == 20 );
-//    // REQUIRE( pv.getiter() == 100 );
-//    // REQUIRE( pv.getratio() == 0.2 );
-//
-//    // // Test adjustpv();
-//    // pv.adjustpv(-0.90);
-//    // REQUIRE( arma::approx_equal(pv.getpv(), initial_mat, "absdiff", 0.0000001) );
-//
-//    // //double y = 1.0 + 1000.0 * pow(0.2 - 0.25, 3);
-//    // //if (y < 0.9) y = 0.9;
-//    // //if (y > 1.1) y = 1.1;
-//    // //if (y == 1.1 | y == 0.9) {
-//    // //  pv = pv % mydiag;
-//    // //}
-//
-//   }
+  pv.addaccept();
+  SECTION( "can add accept" ) {
+
+    REQUIRE( pv.getaccept() == 1   );
+    REQUIRE( pv.getiter()   == 2   );
+    REQUIRE( pv.getratio()  == 0.5 );
+  }
+
+  pv.resetratio();
+  SECTION( "can reset" ) {
+
+    REQUIRE( pv.getaccept() == 0 );
+    REQUIRE( pv.getiter()   == 0 );
+    REQUIRE( pv.getratio()  != pv.getratio() );
+  }
+
+
+  SECTION( "2-parameter version, core functions." ) {
+
+    // Initialize object for testing
+    arma::vec initial_pvs = { 0.7, 0.1 };
+    ProposalVariance2p pv(initial_pvs, 500, 25000, 0.25);
+    arma::mat initial_mat = pv.getpv();
+
+    int i;
+    for (int i = 0; i < 100; i++) {
+      if (i % 4 == 0) pv.addaccept();
+      else (pv.addreject());
+    }
+
+    REQUIRE( pv.getaccept() == 25 );
+    REQUIRE( pv.getiter() == 100 );
+    REQUIRE( pv.getratio() == 0.25 );
+
+    // Test adjustpv();
+    pv.adjustpv(-0.90);
+    REQUIRE( arma::approx_equal(pv.getpv(), initial_mat, "absdiff", 0.0000001) );
+
+    // game out case scenarios where > 0.9 and < 1.1.
+    //double y = 1.0 + 1000.0 * pow(0.2 - 0.25, 3);
+    //if (y < 0.9) y = 0.9;
+    //if (y > 1.1) y = 1.1;
+    //if (y == 1.1 | y == 0.9) {
+    //  pv = pv % mydiag;
+    //}
+
+   }
 
 }
