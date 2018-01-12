c99f11657bfa866fa14512ecdf43156783cd9df8
Matt [MtEvansMacPro] <mjmulvahill@gmail.com>
2018-01-11 20:50:59 -0700

    moving likelihood functions to patient object.

--- include/patient.h
+++ include/patient.h
@@ -35,6 +35,16 @@ using namespace Rcpp;
 struct OnlyPatient {
 //: MCMCSamplingUnit
 
+  // need access to pulses, pulse_count, priors, data.. basically all of this
+  PatientData *data;
+  PatientPriors_Single *priors;
+  PatientEstimates_Single *estimates;
+  std::list<PulseEstimate> pulses;
+  std::list<PulseEstimate>::iterator piter = pulses.begin();
+  std::list<PulseEstimate> responses;
+  std::list<PulseEstimate>::iterator riter = responses.begin();
+
+  // Constructor
   OnlyPatient(PatientData *in_data,
               PatientPriors_Single *in_priors,
               PatientEstimates_Single *in_parms) {
@@ -45,40 +55,96 @@ struct OnlyPatient {
 
   }
 
+  // Get current number of pulses
   int get_pulsecount() { return pulses.size(); };
 
-  // need access to pulses, pulse_count, priors, data.. basically all of this
-  PatientData *data;
-  PatientPriors_Single *priors;
-  PatientEstimates_Single *estimates;
-  std::list<PulseEstimate> pulses;
-  std::list<PulseEstimate>::iterator piter = pulses.begin();
-  std::list<PulseEstimate> responses;
-  std::list<PulseEstimate>::iterator riter = responses.begin();
+
+  // likelihood()
+  //   computes the current likelihood using the observed log-concentrations and
+  //   mean concentration
+  double likelihood(Patient *patient,
+                    Pulse *pulse_excluded,
+                    bool response_hormone = FALSE){
+
+    int i;
+    int N = get_pulsecount();  // function not yet defined **
+    double like = 0; // likelihood to be calculated
+    arma::vec mean_conc;
+    arma::vec *data;
+
+    if (response_hormone) {
+      data = patient->data.response_concentration;
+    } else {
+      data = patient->data.concentration;
+    }
+
+    // Sum across mean_contribs
+    mean_conc = mean_concentration(patient, pulse_excluded);
+    for (i = 0; i < N; i++) {
+      like += pow(data(i) - mean_conc(i), 2);  // should be able to get rid of the loop by element diffing these vectors and squaring
+    }
+
+    like /= (-2.0 * patient->estimates.errorsq);
+    like += -0.5 * N * (1.8378771 + patient->estimates.logerrorsq);
+
+    return like;
+
+  }
+
+  // mean_concentration()
+  //   this takes each pulse's mean_contrib vector and sums across them
+  arma::vec mean_concentration(Pulse **patient, // list of pulses
+                               Pulse *pulse_excluded,
+                               int pulse_count)
+  {
+
+    int i;
+    Pulse *pulse;
+    int N = pulse_count; //patient.get_pulsecount();  // function not yet defined **
+    arma::vec mean_conc(N);
+
+    // Add the contribution to the mean from each pulse
+    pulse = Patient->pulses->next; // move to first pulse, not sure if works**
+    while (pulse != NULL) {
+      if (pulse != pulse_excluded) {
+        for (i = 0; i < N; i++)
+          mean_conc(i) += pulse->mean_contrib(i); // mean contrib not yet fully defined **
+      }
+      node = pulse->next;
+    }
+
+    // Add the baseline contribution and log
+    mean_conc += baseline;
+    mean_conc = log(mean_conc);
+
+    return mean_conc;
+  }
+
+
+
 
 };
 
 
+
+
 //
-// aPatient struct
+// OnePatient struct
 //  For pop model (OnePatient of many) 
 //
 struct OnePatient {
 //: MCMCSamplingUnit
 
   OnePatient(PatientData *in_data,
-             //PatientPriors_Pop *in_priors,
              PatientEstimates_Pop *in_parms) {
 
     data = in_data;
-    //priors = in_priors;
     estimates = in_parms;
 
   }
 
   int get_pulsecount() { return pulses.size(); };
 
-  // need access to pulses, pulse_count, priors, data.. basically all of this
   PatientData *data;
   PatientEstimates_Pop *estimates;
   std::list<PulseEstimate> pulses;
