23f5f48343603a873b2429c62093751974294e6b
Max McGrath <max.a.mcgrath@gmail.com>
2021-07-27 16:31:38 -0600

    Delete unused files

--- R-package/R/pulsatile-package.R
+++
@@ -1,9 +0,0 @@
-#' @title Pulsatile package documentation
-#' @name pulsatile
-#' @description Bayesian deconvolution model for the analysis of time-series of
-#' pulsatile hormone data. See the help files for the primary functions
-#' pulse_spec() and fit_pulse().
-#'
-#' @docType package
-#' @aliases pulsatile pulsatile-package 
-NULL
